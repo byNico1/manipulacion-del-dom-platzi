11cf633a8f4d28c3152461b766a1c3f2c40f9210
bynico1 <nicofoque0205@gmail.com>
2022-09-15 19:57:18 -0500

    Deploy

---
+++ script.js
@@ -0,0 +1,13 @@
+const h1 = document.querySelector("h1");
+const form = document.querySelector("#form");
+const input1 = document.querySelector("#calculo1");
+const input2 = document.querySelector("#calculo2");
+const btn = document.querySelector("#btnCalcular");
+const pResult = document.querySelector("#result");
+
+btn.addEventListener("click", sumarInputValues);
+
+function sumarInputValues(event) {
+  const sumaInputs = input1.value + input2.value;
+  pResult.innerText = "Resultado: " + sumaInputs;
+}
